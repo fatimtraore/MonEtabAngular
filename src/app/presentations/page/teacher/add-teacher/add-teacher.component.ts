b83e2848a5cfcfbb1b2ac25ecd68e552423f1c3a
fatimtraore <fatytraore997@gmail.com>
2024-10-30 16:24:20 +0100

    mise à jour

--- src/app/presentations/page/teacher/add-teacher/add-teacher.component.ts
+++ src/app/presentations/page/teacher/add-teacher/add-teacher.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {BaseService} from '../../../../core/services/base-services/base.service';
 
 @Component({
   selector: 'app-add-teacher',
   standalone: true,
-  imports: [],
+    imports: [
+        NgIf,
+        ReactiveFormsModule
+    ],
   templateUrl: './add-teacher.component.html',
   styleUrl: './add-teacher.component.css'
 })
-export class AddTeacherComponent {
+export class AddTeacherComponent implements OnInit{
+  forms!:FormGroup
+
+  constructor(private baseService: BaseService ) {
+  }
+
+  isInvalidate(input: FormControl){
+    return input.invalid;
+  }
+
+  saveData() {
+
+  }
+
+  isInvalidateInput(control: AbstractControl): boolean {
+    return control.invalid && (control.touched || control.dirty);
+  }
+
+
+
+  ngOnInit() {
+    console.log("mon log est :", {name:'test'});
+    this.forms = new FormGroup({
+      firstName: new FormControl("", [Validators.required]),
+      lastName: new FormControl("", [Validators.required]),
+      matricule: new FormControl("", [Validators.required]),
+      ville: new FormControl("", [Validators.required]),
+      gender: new FormControl("", [Validators.required])
+    });
+  }
+
+}{
 
 }
--- src/app/presentations/page/user/add-user/add-user.component.ts
+++ src/app/presentations/page/user/add-user/add-user.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {BaseService} from '../../../../core/services/base-services/base.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css'
 })
-export class AddUserComponent {
+export class AddUserComponent implements OnInit{
+  forms!:FormGroup
+
+  constructor(private baseService: BaseService ) {
+  }
+
+  isInvalidate(input: FormControl){
+    return input.invalid;
+  }
+
+  saveData() {
+
+  }
+
+  isInvalidateInput(control: AbstractControl): boolean {
+    return control.invalid && (control.touched || control.dirty);
+  }
+
+
+
+  ngOnInit() {
+    console.log("mon log est :", {name:'test'});
+    this.forms = new FormGroup({
+      pseudo: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+      addPassword: new FormControl("", [Validators.required])
+    });
+  }
+
+}{
 
 }
